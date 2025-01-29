bf6caf5948320b7401e08efca755e3c2eadad968
Hariharan2716 <hariharan.m2716@gmail.com>
2025-01-29 16:57:19 +0530

    Added .env to .gitignore and removed from tracking

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require("dotenv").config();
+
+if(process.env.NODE_ENV === "production"){
+  console.log("Running in Production Mode");
+
+} else if (process.env.NODE_ENV === "development"){
+  console.log("Runnig in Development mode");
+}else{
+  console.log("Unknown Environment Mode");
+}
+
+console.log("Server running on port:",process.env.PORT);
+console.log("Your API key:", process.env.API_KEY);
+console.log("Environment:", process.env.NODE_ENV);
+
 const express = require('express');
 const { resolve } = require('path');
 
